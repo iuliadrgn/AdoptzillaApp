b5c85b4df73edc96a2cb0f316a3d619b3ac5ca39
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2022-01-12 19:26:23 +0200

    Added pets ads to database

---
+++ app/src/main/java/Entity/Member.java
@@ -0,0 +1,16 @@
+package Entity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Member extends AppCompatActivity {
+    private String petType;
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+}
+
